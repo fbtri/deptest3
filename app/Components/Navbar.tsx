6a45888fa9490a465ee077fb827737e36ae44cd6
Your Name <you@example.com>
2024-04-11 10:20:53 -0500

    Your commit message here

--- app/Components/Navbar.tsx
+++ app/Components/Navbar.tsx
@@ -1,22 +1,23 @@
 import { NAV_LINKS } from "@/Constants";
 import Image from "next/image";
 import Link from "next/link";
+import React, { useState } from 'react';
 
 const Navbar = () => {
   return (
     <nav className="navbar fixed top-0 left-10 right-6 z-50 bg-transparent py-5">
       <div className="max-container padding-container flex justify-between items-center">
         <Link href="/" legacyBehavior>
-          <div className="cursor-pointer hidden lg:flex">
+          <div className="cursor-pointer">
             <Image src="/Mac App iconW.png" alt="logo" width={70} height={70} />
           </div>
         </Link>
 
         {/* Desktop navigation */}
-        <ul className="hidden lg:flex flex-wrap">
+        <ul className="flex flex-wrap">
           {NAV_LINKS.map((link) => (
             <li key={link.key} className="mr-5">
-              <Link href={link.href} className="text-white hover:text-gray-300 transition-all font-medium">
+              <Link href={link.href} className="text-white hover:text-gray-300 transition-all font-small">
                 {link.label}
               </Link>
             </li>
@@ -24,17 +25,12 @@ const Navbar = () => {
         </ul>
 
         {/* Mobile navigation */}
-        <div className="lg:hidden">
+        <div className="hidden">
           
-          <ul className="flex flex-col mt-5 ml-0">
-          <Link href="/" legacyBehavior>
-          <div className="cursor-pointer">
-            <Image src="/Mac App iconW.png" alt="logo" width={40} height={15} />
-          </div>
-        </Link>
+          <ul className="flex flex-wrap mt-5 ml-0">
             {NAV_LINKS.map((link) => (
-              <li key={link.key} className="mb-3">
-                <Link href={link.href} className="text-white hover:text-gray-300 transition-all font-medium">
+              <li key={link.key} className="mb-0">
+                <Link href={link.href} className="text-white hover:text-gray-300 transition-all font-small">
                   {link.label}
                 </Link>
               </li>
