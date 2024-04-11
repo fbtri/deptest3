086386f2660d759a76e13459d7b47eec272f9961
Your Name <you@example.com>
2024-04-11 16:01:58 -0500

    Your commit message here

--- app/weekend/page.tsx
+++ app/weekend/page.tsx
@@ -1,15 +1,21 @@
 import Navbar from "../Components/Navbar";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Weekend() {
   return (
     <div style={{ backgroundColor: 'black', minHeight: '100vh', padding: '00px', display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
-      <Navbar />
+      <Link href="/" legacyBehavior>
+          <div className="cursor-pointer">
+            <Image src="/Mac App iconW.png" alt="logo" width={70} height={70} />
+          </div>
+        </Link>
+        <h1 style={{ color: 'white' }}> The Weekend </h1>
       <div style={{ maxWidth: '100vw', width: '100%', display: 'flex', justifyContent: 'space-between' }}>
-        {[...Array(5)].map((_, columnIndex) => ( // Iterate over 5 columns
-          <div key={columnIndex} style={{ width: '19%', marginLeft: '0' }}> {/* Adjust width for 5 columns */}
-            {[1, 2, 3, 4].map((index) => { // Iterate over 4 photos in each column
-              const photoIndex = columnIndex * 4 + index; // Calculate photo index
+        {[...Array(4)].map((_, columnIndex) => ( // Iterate over 4 columns
+          <div key={columnIndex} style={{ width: '24%', marginLeft: '0' }}> {/* Set width to 23% for 4 columns */}
+            {[0, 1, 2, 3, 4].map((index) => { // Iterate over 5 photos in each column
+              const photoIndex = columnIndex + index * 4; // Calculate photo index
               return (
                 photoIndex <= 19 && // Check if photo index is within range
                 <div key={index} style={{ marginBottom: '20px' }}>
