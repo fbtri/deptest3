301f76e70becf0f69f1b3895e62dc6baa8c0c34b
Your Name <you@example.com>
2024-04-11 15:43:46 -0500

    Your commit message here

--- app/weekend/page.tsx
+++ app/weekend/page.tsx
@@ -6,30 +6,23 @@ export default function Weekend() {
     <div style={{ backgroundColor: 'black', minHeight: '100vh', padding: '00px', display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
       <Navbar />
       <div style={{ maxWidth: '100vw', width: '100%', display: 'flex', justifyContent: 'space-between' }}>
-        {[1, 2, 3, 4].map((columnIndex) => (
-          <div key={columnIndex} style={{ width: '24.5%', marginLeft: '00px' }}>
-            {columnIndex === 5 && columnIndex * 4 > 19 ? null : (
-              <div style={{ marginBottom: '10px' }}>
-                <Image
-                  src={`/TWK${Math.min(columnIndex * 4, 19)}.jpeg`}
-                  alt={`background image ${Math.min(columnIndex * 4, 19)}`}
-                  layout="responsive"
-                  width={800}
-                  height={600}
-                />
-              </div>
-            )}
-            {[1, 2, 3].map((index) => (
-              <div key={index} style={{ marginBottom: '20px' }}>
-                <Image
-                  src={`/TWK${(columnIndex - 1) * 4 + index}.jpeg`}
-                  alt={`background image ${(columnIndex - 1) * 4 + index}`}
-                  layout="responsive"
-                  width={800}
-                  height={600}
-                />
-              </div>
-            ))}
+        {[...Array(5)].map((_, columnIndex) => ( // Iterate over 5 columns
+          <div key={columnIndex} style={{ width: '19%', marginLeft: '0' }}> {/* Adjust width for 5 columns */}
+            {[1, 2, 3, 4].map((index) => { // Iterate over 4 photos in each column
+              const photoIndex = columnIndex * 4 + index; // Calculate photo index
+              return (
+                photoIndex <= 19 && // Check if photo index is within range
+                <div key={index} style={{ marginBottom: '20px' }}>
+                  <Image
+                    src={`/TWK${photoIndex}.jpeg`}
+                    alt={`background image ${photoIndex}`}
+                    layout="responsive"
+                    width={800}
+                    height={600}
+                  />
+                </div>
+              );
+            })}
           </div>
         ))}
       </div>
