175d037eff2ad05e0924b1f746728cc56ab64daf
Felipe Braga <felipebraga@Felipes-MBP.lan>
2024-04-11 01:43:15 -0500

    Your commit message

--- Constants/index.ts
+++ Constants/index.ts
@@ -1,10 +1,10 @@
 // NAVIGATION
 export const NAV_LINKS = [
-    { href: '/', key: 'home', label: 'Home' },
-    { href: '/', key: 'how_hilink_work', label: 'How Hilink Work?' },
-    { href: '/', key: 'services', label: 'Services' },
-    { href: '/', key: 'pricing ', label: 'Pricing ' },
-    { href: '/', key: 'contact_us', label: 'Contact Us' },
+    { href: '/', key: 'home', label: 'HomeThe Weekend' },
+    { href: '/', key: 'how_hilink_work', label: 'Race Photos' },
+    { href: '/', key: 'services', label: 'Race Course' },
+    { href: '/', key: 'pricing ', label: 'Performance' },
+    { href: '/', key: 'contact_us', label: 'Contact' },
     ];
     // CAMP SECTION
     export const PEOPLE_URL = [
